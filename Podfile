f5dfdb3218f3919d47dfdb87aa57be1b3c1a21b5
Shi Yu <shi.yu@inforichjapan.com>
2020-12-04 14:54:39 +0900

    f c

---
+++ Podfile
@@ -0,0 +1,7 @@
+target 'test302' do
+
+use_frameworks!
+
+pod 'WKCookieWebView', '~> 2.0'
+
+end
--- test302/ViewController.swift
+++ test302/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import WebKit
+import WKCookieWebView
 
 class ViewController: UIViewController {
+    
+    var wv: WKCookieWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        WKCookieWebView.preloadWithDomainForCookieSync(urlString: "api.pre.chargespot.cn") {
+            print(111)
+        }
+        
+        wv = WKCookieWebView(frame: self.view.frame)
+        self.view.addSubview(wv)
+        //加载网页
+        wv.load(NSURLRequest(url:NSURL.init(string:"https://api.pre.chargespot.cn/v1/tpoint/member/bind/6490?agreeDeduct=1")! as URL) as URLRequest)
+        
     }
-
-
+    
 }
-
