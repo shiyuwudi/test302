6d17a6fd8092e0413d5269599d91ca6b308ede7c
Shi Yu <shi.yu@inforichjapan.com>
2020-12-04 13:40:14 +0900

    Initial Commit

---
+++ test302/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test302
+//
+//  Created by shi.yu on 2020/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
